b6773ce5e722061d19aca3247e8bce616bcf2aaa
Ombeline Leclerc-Istria <o.leclerc.istria@gmail.com>
2019-02-06 22:33:51 -0500

    Mouvements perso + init enemy

---
+++ SIDI/Assets/Scripts/Enemy/IEnemy.cs
@@ -0,0 +1,8 @@
+﻿using UnityEngine;
+
+public interface IEnemy
+{
+    void PerformAttack(int damage);
+    void TakeDamage(int amount, Transform obj);
+    void Die();
+}
---
+++ SIDI/Assets/Scripts/Enemy/Target.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Target : MonoBehaviour, IEnemy
+{
+    public int maxHealth;
+    private int currentHealth;
+
+    public float knockbackForce;
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        currentHealth = maxHealth;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    public void PerformAttack(int damage)
+    {
+        // Ne fait rien pour un target dummy
+    }
+
+    public void TakeDamage(int amount, Transform obj)
+    {
+        // Knockback
+        Vector2 knockbackDir = (transform.position - obj.transform.position).normalized;
+        rb.AddForce(knockbackDir * knockbackForce, ForceMode2D.Impulse);
+
+
+        // Réduction de vie
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+            Die();
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
