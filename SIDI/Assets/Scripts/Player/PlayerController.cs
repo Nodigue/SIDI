8f78d917aef1e9d896d003ae7fd9bd479f7c14e5
Nicolas RAPHANAUD <nicolas.raphanaud@gmail.com>
2019-02-11 21:14:04 +0100

    Mouvement Perso + Saut Quasi ok

---
+++ SIDI/Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [Header("Orientation")]
+    
+    [SerializeField] private bool isFacingRight = true;      //Si le personnage est tourné vers la droite
+
+    [Header("Control")]
+    
+    [SerializeField] private float speed = 1f;               //Vitesse (marche)
+    [SerializeField] private float runningSpeed = 2f;        //Vitesse (course)
+    [SerializeField] private float crouchingSpeed = 0.5f;    //Vitesse (accroupi)
+
+    [SerializeField] private float jumpForce = 400f;         //Puissance du saut
+
+    [SerializeField] private bool airControl = false;        //Si le personnage peut être contrôler dans les airs
+
+    [Header("Ground")]
+    
+    [SerializeField] private LayerMask groundMask;           //Masque du Sol
+    [SerializeField] private Transform groundCheck;          //Point de vérification du sol
+    [SerializeField] private bool isGrounded;                //Si le personnage au sol
+
+    [Header("Collision")]
+    
+    [SerializeField] private float radius = 0.2f;            //Rayon du cercle de collision
+
+    private Rigidbody2D rigidBody;                           //RigidBody2D du personnage
+
+    private void Awake()
+    {
+        rigidBody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        //On regarde tous les colliders présent dans le cercle de collision sur le Layer du Sol
+        Collider2D[] colliders = Physics2D.OverlapCircleAll(groundCheck.position, radius, groundMask);
+
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            //Si il y en a au moins un qui n'est pas le personnage, c'est qu'il est au sol
+            if (colliders[i].gameObject != this.gameObject)
+            {
+                isGrounded = true;
+            }
+        }
+    }
+
+    public void Move(float movement, bool jump)
+    {
+        if (isGrounded || airControl)
+        {
+            Vector2 velocity = new Vector2(movement * speed, rigidBody.velocity.y);
+            rigidBody.velocity = velocity;
+
+            if (movement > 0 && !isFacingRight)
+            {
+               Flip();
+            }
+            else if (movement < 0 && isFacingRight)
+            {
+               Flip();
+            }
+        }
+
+        if (isGrounded && jump)
+        {
+            isGrounded = false;
+            rigidBody.AddForce(new Vector2(0f, jumpForce));
+        }
+    }
+
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+
+        Vector2 flip = transform.localScale;
+        flip.x *= -1;
+        transform.localScale = flip;
+    }
+}
--- SIDI/Assets/Scripts/Player/PlayerMovement.cs
+++ SIDI/Assets/Scripts/Player/PlayerMovement.cs
@@ -4,57 +4,22 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    public float speed = 5f;
-    public float jumpPower = 300f;
+    public PlayerController controller;
+    private float horizontalInput = 0f;
 
-    private Rigidbody2D rb;
+    private bool jump = false;
 
-    private LayerMask groundLayerMask;
-    private float lastDistance;
+    private void Update()
+    {
+        horizontalInput = Input.GetAxisRaw("Horizontal") * 10f;
 
-    void Awake()
-    {
-        rb = GetComponent<Rigidbody2D>();
-        groundLayerMask = LayerMask.GetMask("Ground");
+        if (Input.GetButtonDown("Jump"))
+            jump = true;
     }
-    
-    void FixedUpdate()
-    {
-        float inputX = Input.GetAxis("Horizontal");
-        float inputY = Input.GetAxis("Vertical");
 
-        rb.velocity = new Vector2(Input.GetAxis("Horizontal") * speed, rb.velocity.y);
-    }
-}
-    // Check if the user is attempting to jump
-    float jumpAxis = Input.GetAxisRaw("Jump");
-    if (jumpAxis != 0 && rb.velocity.y <= 0)
+    private void FixedUpdate()
     {
-        // Raycast from the feet of the player directly down (or the origin, doesn't matter)
-        RaycastHit2D hit2D = Physics2D.Raycast(rb.position - new Vector2(0f, 0.5f), Vector2.down, 0.2f, environmentLayerMask);
-        // If the raycast hit something
-        if (hit2D)
-        {
-            // Check if the distance of the object hit is less than the last distance checked
-            if (hit2D.distance < lastDistance)
-            {
-                // Update the last distance if the object below is less than the last known distance
-                lastDistance = hit2D.distance;
-            }
-            else
-            {
-                // If the hit distance is not less than the lass distance, then jump (he isn't going to go any lower)
-                lastDistance = 100f;
-                Jump(jumpAxis * jumpForce * Time.deltaTime);
-            }
-        }
+        controller.Move(horizontalInput * Time.fixedDeltaTime, jump);
+        jump = false;
     }
-}
-void Jump(float force)
-{
-    if (force < 0)
-    {
-        return;
-    }
-    rb.AddForce(new Vector2(0, force));
 }
